97d7a766e6b02bbf67a2d02592f99b52738b3261
Pumafron <pumafron@gmail.com>
2021-12-10 15:31:52 -0600

    class user fix

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 module.exports = class Usuarios {
-    constructor(name = "",id= 0,calification = 0){
+    constructor(name = "",id= "",calification = 0){
         this.name = name;
         this.id = id;
         this.calification = calification;
